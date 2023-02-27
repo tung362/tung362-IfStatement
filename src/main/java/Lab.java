e97abb108c1074d62ea27108439258b3efe0f749
Tung Nguyen <7319tnguyen040@gmail.com>
2023-02-27 23:56:22 +0000

    Completed lab

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -19,7 +19,8 @@ public class Lab {
      * @param y second number to be compared.
      * @return x if bool is true. if bool is false, return y.
      */
-    public int ifExample(boolean bool, int x, int y){
-        return 0;
+    public int ifExample(boolean bool, int x, int y)
+    {
+        return bool ? x : y;
     }
 }
